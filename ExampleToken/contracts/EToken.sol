b6af39dd64c0b80dea7b64d76983c96340a5b2ae
coditech0926 <coditech.super926@gmail.com>
2018-06-06 15:15:24 +0800

    Create EToken.sol

---
+++ ExampleToken/contracts/EToken.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.24;
+
+import 'openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
+
+contract TToken is StandardToken {
+    string public constant name = 'Example Token';
+    string public constant symbol = "ET";
+    uint8 public constant decimals = 2;
+    uint constant _initialSupply = 2100000000;
+
+    constructor() public {
+        totalSupply_ = _initialSupply;
+        balances[msg.sender] = _initialSupply;
+        emit Transfer(address(0), msg.sender, _initialSupply);
+    }
+}
