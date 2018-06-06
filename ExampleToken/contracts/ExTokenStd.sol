d6ac283c27a22a65c76f20c22e2802075f64a6c0
coditech0926 <coditech.super926@gmail.com>
2018-06-06 17:23:03 +0800

    Update and rename EToken.sol to ExTokenStd.sol

--- ExampleToken/contracts/ExTokenStd.sol
+++ ExampleToken/contracts/ExTokenStd.sol
@@ -2,9 +2,9 @@ pragma solidity ^0.4.24;
 
 import 'openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
 
-contract TToken is StandardToken {
+contract ExToken is StandardToken {
     string public constant name = 'Example Token';
-    string public constant symbol = "ET";
+    string public constant symbol = "ExT";
     uint8 public constant decimals = 2;
     uint constant _initialSupply = 2100000000;
 
