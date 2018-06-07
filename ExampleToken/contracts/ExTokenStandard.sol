0bd0253dca12e2e33fdc6356c717f31706504ea9
coditech0926 <coditech.super926@gmail.com>
2018-06-07 19:59:52 +0800

    Update ExTokenStandard.sol

--- ExampleToken/contracts/ExTokenStandard.sol
+++ ExampleToken/contracts/ExTokenStandard.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.24;
 
-// import using absolute path is also working for 'truffle compile', but it will be very tricky to compile using 'solc'  
+// import using absolute path is working for 'truffle compile', but it will be very tricky to compile using 'solc'  
 import '../node_modules/openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
 // import 'openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol'; 
 
